b797cecba2cd33515ce49e5c27900699c25c3481
pengchao <2477433191@qq.com>
2025-10-30 19:56:00 +0800

    demo1

---
+++ src/main/java/com/example/pengaiagent/PengAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.pengaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PengAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pengaiagent/PengAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pengaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
